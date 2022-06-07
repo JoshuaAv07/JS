dbb35435c6423c0c5d73a9c0308a5c9d08a710f6
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-07 07:22:12 -0600

    1st commit

---
+++ hello_world/js/01_hello_world.js
@@ -0,0 +1,5 @@
+alert("JS Hello World");
+alert("Welcome");
+
+/*console.log("Show this in console");
+console.log(88+4);*/
---
+++ hello_world/js/02_variables.js
@@ -0,0 +1,12 @@
+// VARIABLES
+// A variable is an information container
+
+var country = "Mexico";
+var continent = "America";
+var antiguedad = 2022;
+var country_and_continet = country +" "+ continent;
+
+country = "España";
+
+console.log(country, continent, antiguedad);
+alert(country_and_continet);
