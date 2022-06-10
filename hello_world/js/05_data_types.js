05171374f36636981b418997010c9b66c3b07187
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-10 09:59:23 -0600

    new data types

--- hello_world/js/05_data_types.js
+++ hello_world/js/05_data_types.js
@@ -9,10 +9,16 @@ alert("Operation's results is: " + operation);
 
 
 // Data types
-var int_num = 44;
+var int_num = 55;
 var text_chain = "Hello, how are you?";
 var true_or_false = false;
 var false_num = "33.4";
+
+console.log(Number(true_or_false) + 7);
+console.log(parseInt("345") + 7);
+console.log(parseFloat(int_num) + 7);
+
+console.log(String(int_num) + 7);
 
 console.log(typeof int_num);
 console.log(typeof text_chain);
