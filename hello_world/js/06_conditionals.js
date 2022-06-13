e6b5f790f7a646001e90753dab5d4a5b170908a0
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-13 08:06:45 -0600

    06

--- hello_world/js/06_conditionals.js
+++ hello_world/js/06_conditionals.js
@@ -3,8 +3,9 @@
 // Conditional
 // If A is equal to B then do something
 
-var age = 18;
-var name = "Joshua Aviles";
+var age = 21;
+var age2 = 12
+var name = "Joshua";
 
 /*
 // Relational Operators
@@ -15,19 +16,44 @@ var name = "Joshua Aviles";
     Equal: ==
     Distinct: !=    
 */
+if (age > age2){
 
-if (age >= 18){
-    // Es mayor de edad
-    console.log(name + " is " + age + " years old");
+    console.log("Age 1 is greater than age 2");
     if (age <= 33){
         console.log("Still Millenial");
-    } else if (age >= 70){
-        console.log("You are a geezer");
-    } else {
-        console.log("Not millenial Anymore")
+    } else{
+        console.log("You are a geeXer");
     }
-
 } else {
-    // Es mayor de edad
     console.log(name + " is " + age + " years old");
+}
+
+if (age >= 18){
+    // Above age
+    console.log(name + " is " + age + " years old | Above age");
+} else {
+    // Under age
+    console.log(name + " is " + age + " years old | Under age");
+}
+
+var year = 2018;
+
+// Negation
+
+if (year != 2021){
+    console.log("Year is not 2021");
+}
+
+// AND
+if (year >= 2000 && year <= 2022 && year !=2018){
+    console.log("We are at the current era");
+} else {
+    console.log("We are at the postmodern era");
+}
+
+// OR
+if (year == 2008 || (year >= 2018 && year == 2028)){
+    console.log("Year ends in 8");
+} else {
+    console.log("NOT REGISTERED");
 }
