de9cce0b5eb98731d69668d23adf481284ac4d6e
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-13 08:34:16 -0600

    07

---
+++ hello_world/js/07_switch.js
@@ -0,0 +1,23 @@
+'use-strict'
+
+
+
+var age = 25;
+var print = "";
+
+switch(edad){
+    case 18:
+        print = "You are barely above age";
+    break;
+    case 25:
+        print = "You are an adult";
+    break;
+    case 40:
+        print = "40s Crisis";
+    break;
+    case 75:
+        print = "Geezer";
+    break;
+    default:
+        print = "Neutral age;"
+}
