08762498697bd96c8eefb8dab2b99ca607def0fc
JoshuaAv07 <a6520150001@utch.edu.mx>
2022-06-22 08:35:00 -0600

    ex8

--- exercises/js/exam_u1.js
+++ exercises/js/exam_u1.js
@@ -2,10 +2,10 @@
 var cont = 0;
 
 for (let i = 1; i <= 100; i++){
-    var n = i.toString()
-    
+    var n = i.toString();
+
     if (n.includes(9)){
-        var cont = cont + 1; 
+        cont += 1; 
         console.log("Nine was found at: " + i);
         document.write(cont + "- Nine was found at: " + i + "<br>");
     }
